d8d8e4dd90d9a5702c832d2b1b766aa17c89459c
Robert Schumann <robert@schumann.link>
2022-09-13 11:09:47 +0200

    fix: annotation file formatting

--- annotation.go
+++ annotation.go
@@ -8,8 +8,8 @@ import (
 
 type PodNatAnnotation struct {
 	PublicInterface bool   `json:"pubif"`
-	SourcePort      uint16    `json:"src"`
-	DestinationPort uint16    `json:"dst"`
+	SourcePort      uint16 `json:"src"`
+	DestinationPort uint16 `json:"dst"`
 	Protocol        string `json:"proto"`
 }
 
@@ -18,7 +18,7 @@ func parseAnnotation(data string) (*PodNatAnnotation, error) {
 	// default values can be overridden by unmarshaling
 	pa := PodNatAnnotation{
 		PublicInterface: true,
-		Protocol: "tcp",
+		Protocol:        "tcp",
 	}
 
 	err := json.Unmarshal([]byte(data), &pa)
